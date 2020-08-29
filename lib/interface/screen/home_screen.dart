49dd49bdce8389b3362a2030a36d01e6dca3e96b
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-08-29 17:30:32 +0200

    Sistemato l'assetto del progetto.

---
+++ lib/interface/screen/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String route = "/homeScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
